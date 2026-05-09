29e9fe0d4bce9e44f9c105c63b5bf3321e801728
floyd121 <floydhlulani121@gmail.com>
2026-05-09 14:08:55 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
-from strenlit_mic_recorder import mic_recorder
+from streamlit_mic_recorder import mic_recorder
 
 # --- APP CONFIG ---
 st.set_page_config(page_title="Zenith AI Planner", page_icon="🤖")
